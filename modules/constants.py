784c0038b740eea129f7cffefc87d95baf8a49eb
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-03-31 16:25:11 +0800

    feat: wip news retreival

---
+++ modules/constants.py
@@ -0,0 +1,5 @@
+PERPLEXITY_API_KEY = 'PERPLEXITY_API_KEY' # the key used to access the perplexity api key in the .env file 
+
+# this is the most powerful model available on the perplexity api
+# thus, it is also the most expensive one. so double check if you want to use this model
+PERPLEXITY_ONLINE_MODEL = 'pplx-70b-online'
--- modules/news_retreival.py
+++ modules/news_retreival.py
@@ -1,11 +1,21 @@
 from langchain_community.chat_models import ChatPerplexity
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
+from constants import PERPLEXITY_API_KEY, PERPLEXITY_ONLINE_MODEL  
 import os
 
 load_dotenv()  # take environment variables from .env.
 
-perplexity_api_key = os.environ.get("PERPLEXITY_API_KEY");
-print(perplexity_api_key)
+PPLXApiKey = os.getenv(PERPLEXITY_API_KEY)
+chat = ChatPerplexity(temperature=0, pplx_api_key=PPLXApiKey, model=PERPLEXITY_ONLINE_MODEL)
+
+system = "You are a helpful assitant whose job is to prvide the latest news from various industries. Give me atleast one paragraph about each of the latest news items."
+human = "{input}"
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+chain = prompt | chat
+response = chain.invoke({"input": "What are some of the biggest news from the crypto industry over the last few days?."})
+print(response.content)
+
 
 
