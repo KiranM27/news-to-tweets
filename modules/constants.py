a853d0135bd74abf70b08f0819ede3a70d0f3c7d
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-03-31 16:39:19 +0800

    refactor: cleaned up some code

--- modules/constants.py
+++ modules/constants.py
@@ -3,3 +3,12 @@ PERPLEXITY_API_KEY = 'PERPLEXITY_API_KEY' # the key used to access the perplexit
 # this is the most powerful model available on the perplexity api
 # thus, it is also the most expensive one. so double check if you want to use this model
 PERPLEXITY_ONLINE_MODEL = 'pplx-70b-online'
+
+# prompts 
+# does not really give an entire paragraph, but might as well put it in the prompt 
+PERPLEXITY_ONLINE_MODEL_SYSTEM_PROMPT = 'You are a helpful assitant whose job is to prvide the latest news from various industries. Give me atleast one paragraph about each of the latest news items.'
+PERPLEXITY_ONLINE_MODEL_HUMAN_PROMPT = "What are some of the biggest news from the {industry} industry over the last few days?"
+
+# industry names
+CRYPTO_INDUSTRY = 'crypto and blockchain'
+AI_INDUSTRY = 'artificial intelligence (ai)'
--- modules/news_retreival.py
+++ modules/news_retreival.py
@@ -1,21 +1,28 @@
 from langchain_community.chat_models import ChatPerplexity
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from constants import PERPLEXITY_API_KEY, PERPLEXITY_ONLINE_MODEL  
+from constants import PERPLEXITY_API_KEY, PERPLEXITY_ONLINE_MODEL , PERPLEXITY_ONLINE_MODEL_SYSTEM_PROMPT, PERPLEXITY_ONLINE_MODEL_HUMAN_PROMPT, CRYPTO_INDUSTRY, AI_INDUSTRY
 import os
 
 load_dotenv()  # take environment variables from .env.
 
-PPLXApiKey = os.getenv(PERPLEXITY_API_KEY)
-chat = ChatPerplexity(temperature=0, pplx_api_key=PPLXApiKey, model=PERPLEXITY_ONLINE_MODEL)
+class NewsRetreival:
+    def __init__(self):
+        pplx_api_key = os.getenv(PERPLEXITY_API_KEY)
+        self.chat = ChatPerplexity(temperature=0, pplx_api_key=pplx_api_key, model=PERPLEXITY_ONLINE_MODEL)
+        self.system = PERPLEXITY_ONLINE_MODEL_SYSTEM_PROMPT
+        self.human = "{input}"
+        self.prompt = ChatPromptTemplate.from_messages([("system", self.system), ("human", self.human)])
+        self.chain = self.prompt | self.chat
 
-system = "You are a helpful assitant whose job is to prvide the latest news from various industries. Give me atleast one paragraph about each of the latest news items."
-human = "{input}"
-prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
-
-chain = prompt | chat
-response = chain.invoke({"input": "What are some of the biggest news from the crypto industry over the last few days?."})
-print(response.content)
-
-
-
+    def get_news(self, industry: str):
+        print(f"[INFO] Retrieving news for the {industry} industry")
+        query = PERPLEXITY_ONLINE_MODEL_HUMAN_PROMPT.format(industry=industry)
+        response = self.chain.invoke({"input": query})
+        print(f"[INFO] Retrieved news for the {industry} industry")
+        return response.content
+    
+if __name__ == "__main__":
+    news = NewsRetreival()
+    crypto_news = news.get_news(CRYPTO_INDUSTRY)
+    ai_news = news.get_news(AI_INDUSTRY)
